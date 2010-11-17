dce7c40135aec2d69787fc5ee9eaa3a79799e034
Andrew Evans <andrew@atevans.com>
2010-11-16 21:55:04 -0800

    Proper win, lose, and draw conditions

--- ttt.rb
+++ ttt.rb
@@ -7,6 +7,16 @@ helpers do
   def new_board
     session['ttt'] = [[0,0,0],[0,0,0],[0,0,0]]
   end
+  
+  def win?(player)
+    @win_conditions.any? { |coordinates| 
+      coordinates.all? {|point| session['ttt'][point[0]][point[1]] == player }
+    }
+  end
+  
+  def open_location?
+    session['ttt'].any? { |row| row.any?{|col| col == 0 } }
+  end
 end
 
 before do
@@ -29,26 +39,43 @@ get '/new' do
   redirect '/'
 end
 
-get '/win' do
-  new_board
-  haml :win
+[:win, :lose, :draw].each do |page|
+  get "/#{page}" do
+    new_board
+    haml page
+  end
 end
 
 post '/' do
   hit = params.keys.first.match(/(\d+),(\d+)/).to_a
   row, col = hit[1].to_i, hit[2].to_i if hit.count > 2
   session['ttt'][row][col] = 1
-  ai_moved = false
-  @win_conditions.each do |coordinates|
-    redirect '/win' if coordinates.all? {|point| session['ttt'][point[0]][point[1]] == 1 }
-  end
+  redirect '/win' if win?(1)
+  redirect '/draw' unless open_location?
   
   ai_moved = false
-  case [:row, :col].sample
-  when :row
-    session['ttt'][row][session['ttt'][row].index(0)] = 2
-  when :col
-    session['ttt'][(0..2).to_a.select {|i| session['ttt'][i][col] == 0}.first][col] = 2
-  end
+  begin
+    # if player is about to win, block them
+    @win_conditions.each do |coordinates|
+      next if ai_moved
+      open_location = coordinates.select{|point| session['ttt'][point[0]][point[1]] == 0}.first
+      if coordinates.select {|point| session['ttt'][point[0]][point[1]] == 1}.count > 1 and open_location
+        session['ttt'][open_location[0]][open_location[1]] = 2
+        ai_moved = true
+      end
+    end
+    
+    #otherwise, move randomly
+    unless ai_moved
+      r, c = (0..2).to_a.sample, (0..2).to_a.sample
+      if session['ttt'][r][c] == 0
+        session['ttt'][r][c] = 2
+        ai_moved = true
+      end
+    end
+  end while (ai_moved == false)
+  
+  redirect '/lose' if win?(2)
+  redirect '/draw' unless open_location?
   haml :index
 end
